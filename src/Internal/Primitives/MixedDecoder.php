9f0de06940b151bb36319fd48e6f4934ba0e3754
Emanuele Menon <emnlmn@gmail.com>
2021-04-09 17:26:03 +0200

    Implemented mixed primitive (#19)

---
+++ src/Internal/Primitives/MixedDecoder.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Facile\PhpCodec\Internal\Primitives;
+
+use Facile\PhpCodec\Decoder;
+use function Facile\PhpCodec\Internal\standardDecode;
+use Facile\PhpCodec\Validation\Context;
+use Facile\PhpCodec\Validation\Validation;
+
+/**
+ * @template U of mixed
+ * @implements Decoder<U, U>
+ */
+class MixedDecoder implements Decoder
+{
+    public function validate($i, Context $context): Validation
+    {
+        return Validation::success($i);
+    }
+
+    public function decode($i): Validation
+    {
+        return standardDecode($this, $i);
+    }
+
+    public function getName(): string
+    {
+        return 'mixed';
+    }
+}
