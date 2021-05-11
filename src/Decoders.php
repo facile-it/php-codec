2b6457219e49bce80eed553379e17157010cc107
Ilario Pierbattista <ilario.pierbattista@facile.it>
2021-05-11 08:58:23 +0200

    Implement map for decoders

--- src/Decoders.php
+++ src/Decoders.php
@@ -5,11 +5,39 @@ declare(strict_types=1);
 namespace Facile\PhpCodec;
 
 use Facile\PhpCodec\Internal\Primitives\UndefinedDecoder;
+use Facile\PhpCodec\Validation\Context;
+use Facile\PhpCodec\Validation\Validation;
 
 final class Decoders
 {
     private function __construct()
     {
+    }
+
+    /**
+     * @template I
+     * @template A
+     * @template B
+     * @psalm-param callable(A):B $f
+     * @psalm-return callable(Decoder<I, A>): Decoder<I, B>
+     *
+     * @param callable $f
+     *
+     * @return callable
+     */
+    public static function map(callable $f): callable
+    {
+        return function (Decoder $da) use ($f): Decoder {
+            return new ConcreteDecoder(
+                /**
+                 * @param I $i
+                 */
+                function ($i, Context $context) use ($f, $da): Validation {
+                    return Validation::map($f, $da->validate($i, $context));
+                },
+                $da->getName()
+            );
+        };
     }
 
     /**
