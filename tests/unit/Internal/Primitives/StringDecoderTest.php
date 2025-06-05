3fc6de81617c810d4f9ec0652e4ecef1fea730e2
Michele Benigni <michele.benigni@finital.it>
2025-06-05 12:23:27 +0200

    fix String Decoder

---
+++ tests/unit/Internal/Primitives/StringDecoderTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\unit\Internal\Primitives;
+
+use Facile\PhpCodec\Internal\Primitives\StringDecoder;
+use Facile\PhpCodec\Validation\Context;
+use Facile\PhpCodec\Validation\ValidationFailures;
+use Facile\PhpCodec\Validation\ValidationSuccess;
+use PHPUnit\Framework\TestCase;
+
+final class StringDecoderTest extends TestCase
+{
+
+    public function testValidString(): void
+    {
+        $decoder = new StringDecoder();
+
+        $result = $decoder->decode('ciao');
+
+        $this->assertInstanceOf(ValidationSuccess::class, $result);
+    }
+
+    /**
+     * @dataProvider invalidValuesProvider
+     */
+    public function testInvalidValues($input): void
+    {
+        $decoder = new StringDecoder();
+        $result = $decoder->decode($input);
+        $this->assertInstanceOf(ValidationFailures::class, $result);
+    }
+
+    public static function invalidValuesProvider(): array
+    {
+        return [
+            'integer' => [123],
+            'float' => [3.14],
+            'boolean' => [true],
+            'array' => [['not', 'a', 'string']],
+            'object' => [new \stdClass()],
+            'null' => [null],
+        ];
+    }
+}
